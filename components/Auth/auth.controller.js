e625f610ec1b9f7b11d93d1cc2b1cff9dcd18cb1
ntantai21810 <nguyentantai9a3@gmail.com>
2021-12-07 22:37:24 +0700

    add layout

--- components/Auth/auth.controller.js
+++ components/Auth/auth.controller.js
@@ -1,5 +1,21 @@
 module.exports = {
-    index: function(req, res, next) {
-        res.render('login', { title: 'Login' });
-    }
-}
+  index: function (req, res, next) {
+    res.render("login", { title: "Login" });
+  },
+
+  listAdmin: function (req, res, next) {
+    res.render("listAdmin", { title: "List admin" });
+  },
+
+  createAdminForm: function (req, res, next) {
+    res.render("createAdmin", { title: "Create admin" });
+  },
+
+  createAdmin: function (req, res, next) {
+    console.log(req.body);
+  },
+
+  detail: function (req, res, next) {
+    res.render("detailAdmin", { title: "Detail" });
+  },
+};
