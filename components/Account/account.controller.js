673332f3402fcca950c0bb60dd396be65e5e3c80
ntantai21810 <nguyentantai9a3@gmail.com>
2021-12-08 22:04:50 +0700

    Add listAdmin, admin detail page

--- components/Account/account.controller.js
+++ components/Account/account.controller.js
@@ -1,23 +1,25 @@
-const authService = require('../Auth/auth.service')
+const authService = require("../Auth/auth.service");
 module.exports = {
-  
-  listAdmin: function (req, res, next) {
-    const Users = authService.showUser();
-    res.render("listAdmin", {Users});
+  listAdmin: async function (req, res, next) {
+    const admins = await authService.showUser();
+    const adminWithKey = admins.map((item, index) => ({
+      ...item,
+      key: index + 1,
+    }));
+    res.render("listAdmin", { adminWithKey });
   },
 
   createAdminForm: function (req, res, next) {
     res.render("createAdmin", { title: "Create admin" });
   },
 
-  createAdmin:async function (req, res, next) {
-    await authService.createAdmin(req,res);
-    res.redirect('/');
+  createAdmin: async function (req, res, next) {
+    await authService.createAdmin(req, res);
+    res.redirect("/");
   },
 
-  detail: function (req, res, next) {
-    admin = authService.findByUsername(req.user.username);
-    res.render("detailAdmin", {admin});
+  detail: async function (req, res, next) {
+    const admin = await authService.findByUsername(req.user.username);
+    res.render("detailAdmin", { admin });
   },
-  
 };
