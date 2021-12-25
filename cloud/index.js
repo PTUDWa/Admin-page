94c773b3e0245a3f464d756d9566624d4aded2db
Nguyen Tai <62838260+ntantai21810@users.noreply.github.com>
2021-12-25 22:53:41 +0700

    upload image

---
+++ cloud/index.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "djiu6mpmw",
+  api_key: "237535461258664",
+  api_secret: "4GEd5D3-fvxUiK4ZPTBvDapFWtA",
+});
+
+module.exports = cloudinary;
