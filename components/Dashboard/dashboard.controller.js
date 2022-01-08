56e2d7c8dcfbd3fcf39006032168fc80815118c7
Nguyen Tai <62838260+ntantai21810@users.noreply.github.com>
2022-01-08 12:04:23 +0700

    add statistic

--- components/Dashboard/dashboard.controller.js
+++ components/Dashboard/dashboard.controller.js
@@ -1,9 +1,69 @@
 const productService = require("../Product/product.service");
 const moment = require("moment");
+const orderService = require("../Order/order.service");
 
 module.exports = {
   index: async function (req, res, next) {
     const top10ProductsBySold = await productService.getTop10ProductsBySold();
+    const quarter = Math.floor((moment().month() + 3) / 3);
+
+    const orders = await Promise.all([
+      orderService.getOrders({
+        status: "ready_for_pickup",
+        createdAt: {
+          $gte: moment().startOf("day").toDate(),
+          $lt: moment().endOf("day").toDate(),
+        },
+      }),
+      orderService.getOrders({
+        status: "ready_for_pickup",
+        createdAt: {
+          $gte: moment().startOf("week").toDate(),
+          $lt: moment().endOf("week").toDate(),
+        },
+      }),
+      orderService.getOrders({
+        status: "ready_for_pickup",
+        createdAt: {
+          $gte: moment().startOf("month").toDate(),
+          $lt: moment().endOf("month").toDate(),
+        },
+      }),
+      orderService.getOrders({
+        status: "ready_for_pickup",
+        createdAt: {
+          $gte: moment().startOf("year").toDate(),
+          $lt: moment().endOf("year").toDate(),
+        },
+      }),
+      orderService.getOrders({
+        status: "ready_for_pickup",
+        createdAt: {
+          $gte: moment(`${(quarter - 1) * 3 + 1}/${moment().year()}`, "MM/YYYY")
+            .startOf("month")
+            .toDate(),
+          $lt: moment(`${(quarter - 1) * 3 + 3}/${moment().year()}`, "MM/YYYY")
+            .endOf("month")
+            .toDate(),
+        },
+      }),
+    ]);
+
+    const totalPrice = orders.map((orders) =>
+      orders.reduce(
+        (price, order) =>
+          price +
+          order.items.reduce(
+            (total, item) => total + item.productID.price * item.qty,
+            0
+          ),
+        0
+      )
+    );
+
+    const [incomeDay, incomeWeek, incomeMonth, incomeYear, incomeQuarter] =
+      totalPrice;
+
     const top10ProductsBySoldWithKey = top10ProductsBySold.map(
       (item, index) => ({
         ...item,
@@ -12,6 +72,14 @@ module.exports = {
       })
     );
 
-    res.render("index", { title: "Dashboard", top10ProductsBySoldWithKey });
+    res.render("index", {
+      title: "Dashboard",
+      top10ProductsBySoldWithKey,
+      incomeDay,
+      incomeWeek,
+      incomeMonth,
+      incomeYear,
+      incomeQuarter,
+    });
   },
 };
